f70fc8538a3e75ee25fc6806a5d0af640c9e8b31
FranklinLMR <rosariosanchezfranklin@gmail.com>
2025-10-01 13:14:33 -0400

    first changes

---
+++ main.sql
@@ -0,0 +1,4 @@
+--Customers that live in Dominican Republic and its city
+SELECT customer.first_name as "Name", customer.last_name as "Last name", city.city AS "City"
+FROM customer JOIN city JOIN country JOIN address
+;
