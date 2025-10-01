d5428c4bbd05d923b7c4c7b32b2ee3b117ebe140
Franklin <rosariosanchezfranklin@gmail.com>
2025-10-01 14:30:38 -0400

    Update 2

--- main.sql
+++ main.sql
@@ -1,4 +1,9 @@
 --Customers that live in Dominican Republic and its city
-SELECT customer.first_name as "Name", customer.last_name as "Last name", city.city AS "City"
+SELECT country.country AS  "Country", COUNT(customer.customer_id) AS "People in that country"
 FROM customer JOIN city JOIN country JOIN address
+WHERE customer.address_id = address.address_id
+AND address.city_id = city.city_id
+AND city.country_id = country.country_id
+GROUP BY country.country_id
+ORDER BY "People in that Country" DESC
 ;
